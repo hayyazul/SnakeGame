3e1f9b05b6aa5c0f39f3c5d641ac09d4ba4c8de2
syed <syed@SYEDHASSANREALT>
2024-06-26 16:57:45 -0500

    Set up basic html, css, and beginnings of javascript

--- SnakeGame/app.js
+++ SnakeGame/app.js
@@ -1,3 +1,25 @@
 'use strict';
 
+var board;
+var context;
+
+var width = 10;
+var height = 10;
+
+// Initializer function.
+window.onload = function () {
+    board = document.getElementById("board");
+    board.width = width;
+    board.height = height; 
+
+    context = board.getContext("2d");
+
+    update()
+};
+
+function update() {
+    context.fillStyle = "black";
+    context.fillRect(0, 0, board.width, board.height)
+}
+
 console.log('Hello world');
